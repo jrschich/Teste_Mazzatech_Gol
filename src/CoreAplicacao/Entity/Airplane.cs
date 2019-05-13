bdc962f95f5b2a38b9c3bf34436af4dc8297d7ee
jrschich <jrschich@hotmail.com>
2019-05-12 21:21:09 -0300

    Add project files.

---
+++ src/CoreAplicacao/Entity/Airplane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreAplicacao.Entity
+{
+    public class Airplane
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+        public int QtdePassageiros { get; set; }
+        public DateTime DataRegistro { get; set; }
+    }
+}
